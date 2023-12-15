beec9b0a267f8dd6e8b0ee94ce17f4afa35ec55d
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-15 13:40:16 +0900

    List<Juice>のインポート

--- src/main/java/com/example/juice/JuiceMapper.java
+++ src/main/java/com/example/juice/JuiceMapper.java
@@ -3,6 +3,8 @@ package com.example.juice;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface JuiceMapper {
     @Select("SELECT * FROM juices")
