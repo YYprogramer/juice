7b969380ee14882d5f4fb78c2eda6174f91f088a
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-15 14:18:51 +0900

    Juiceテーブルのデータを増やす

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -8,4 +8,10 @@ CREATE TABLE juices (
 );
 
 INSERT INTO juices (name, price) VALUES ("コカ・コーラ", 180);
-INSERT INTO juices (name, price) VALUES ("コーヒー", 100);
+INSERT INTO juices (name, price) VALUES ("バブルマン", 120);
+INSERT INTO juices (name, price) VALUES ("ペプシ", 150);
+INSERT INTO juices (name, price) VALUES ("ファンタ", 130);
+INSERT INTO juices (name, price) VALUES ("レッドブル", 200);
+INSERT INTO juices (name, price) VALUES ("モンスター", 220);
+INSERT INTO juices (name, price) VALUES ("オロナミンC", 110);
+INSERT INTO juices (name, price) VALUES ("リポビタンD", 140);
