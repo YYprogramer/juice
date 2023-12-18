529b8f3b3d5374cd2db6bb77d2feea63c53526cb
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-19 04:54:18 +0900

    Serviceクラスの作成

--- src/main/java/com/example/juice/JuiceService.java
+++ src/main/java/com/example/juice/JuiceService.java
@@ -1,4 +1,19 @@
 package com.example.juice;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class JuiceService {
+    private final JuiceMapper juiceMapper;
+
+    public JuiceService(JuiceMapper juiceMapper) {
+        this.juiceMapper = juiceMapper;
+    }
+
+    public Juice findJuice(int id) {
+        Optional<Juice> juice = this.juiceMapper.findById(id);
+        return juice.get();
+    }
 }
