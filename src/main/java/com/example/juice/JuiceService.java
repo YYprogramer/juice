766b8ab78ed8729d3b3fdd6ccc1d56c1d6ef5855
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-19 04:42:09 +0900

    Serviceクラスの作成場所がおかしかったので一旦削除

--- src/main/java/com/example/juice/JuiceService.java
+++ src/main/java/com/example/juice/JuiceService.java
@@ -1,15 +1,4 @@
 package com.example.juice;
 
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-    private final JuiceMapper juiceMapper;
-    public JuiceService(JuiceMapper juiceMapper) {
-        this.juiceMapper = juiceMapper;
-    }
-    public User findUser(int id) {
-        Optional<User> user = this.juiceMapper.findById(id);
-        return user.get(); // Optional.get()は値が存在しない場合例外を発⽣させる
-    }
+public class JuiceService {
 }
