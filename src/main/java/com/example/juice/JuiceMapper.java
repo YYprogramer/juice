3127b4cfe47b3d1a6d34f2caead52331b18c3a92
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-19 05:25:46 +0900

    Mapperの中でIDを使用して検索できるように修正

--- src/main/java/com/example/juice/JuiceMapper.java
+++ src/main/java/com/example/juice/JuiceMapper.java
@@ -1,6 +1,7 @@
 package com.example.juice;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -12,6 +13,6 @@ public interface JuiceMapper {
     List<Juice> findAll();
     @Select("SELECT * FROM juices WHERE name LIKE CONCAT(#{prefix}, '%') AND name LIKE CONCAT('%',#{suffix}) AND name LIKE CONCAT('%',#{contains},'%')")
     List<Juice> findByNameStartingWith(String prefix, String suffix, String contains);
-
-    Optional<Juice> findById(int id);
+    @Select("SELECT * FROM juices WHERE id = #{id}")
+     Optional<Juice> findById(@Param("id") int id);
 }
