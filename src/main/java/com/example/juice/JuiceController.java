68b6dd6f6b79140d004c7276783b0b551d2f83da
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2023-12-20 04:50:39 +0900

    JuiceNotFoundExceptionが呼び出された時のメソッドをControllerに記載

--- src/main/java/com/example/juice/JuiceController.java
+++ src/main/java/com/example/juice/JuiceController.java
@@ -1,11 +1,16 @@
 package com.example.juice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class JuiceController {
@@ -17,5 +22,17 @@ public class JuiceController {
     @GetMapping("/juices/{id}")
     public Juice getJuice(@PathVariable("id") int id) {
         return juiceService.findJuice(id);
+    }
+
+    @ExceptionHandler(value = JuiceNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleJuiceNotFoundException(
+            JuiceNotFoundException e, HttpServletRequest request) {
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status",String.valueOf(HttpStatus.NOT_FOUND.value()),
+                "error",HttpStatus.NOT_FOUND.getReasonPhrase(),
+                "message",e.getMessage(),
+                "path",request.getRequestURI());
+                return new ResponseEntity(body, HttpStatus.NOT_FOUND);
     }
 }
